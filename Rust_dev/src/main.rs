b5c755126aa66de24e1a9222d021fdc9fa565af0
sabuj golui <sabujgolui5@gmail.com>
2023-07-09 12:33:46 +0530

    variables in rust

--- Rust_dev/src/main.rs
+++ Rust_dev/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    //variable in rust
+    // unsigned integer
+    // u8, u16, u32, u64, u128
+
+    let unsigned = 10;
+
+    // signed integer
+    // 1B, 116, 132,
+
+    let signed = -10;
+
+    // float is used for decimals 1.4
+
+    let float = 1.023;
+
+    println!(
+        "Unsigned : {}, Signed: {}, Float : {}",
+        unsigned, signed, float
+    );
+
+    // char - can only be
+    let letter = "c";
+    let emoji = "\u{1F600}";
+
+    println!("letter : {}, Emoji : {}", letter, emoji);
 }
